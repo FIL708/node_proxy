b587e43088c0b9dd95a8ed1c7a2cef6c1ed09232
Filip Zebrowski <f.zebrowski@godeltech.com>
2024-04-12 14:18:59 +0200

    PROXY-3: using env variables

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const https = require('https')
+const config = require('config')
 const axios = require('axios')
+
+const key = config.get("API_KEY")
+const url = config.get("NASA_URL")
+const fullUrl = `${url}feed?start_date=2024-04-01&end_date=2024-04-05&api_key=${key}`
 
 const httpsAgent = new https.Agent({
     rejectUnauthorized: false
   });
 
-const URL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-04-01&end_date=2024-04-05&api_key=8myaceLy6jnnDfZjLKb610ktfDpPc9ZKGcKMJbeX"
 
-axios.get(URL, { httpsAgent })
+axios.get(fullUrl, { httpsAgent })
     .then(res => console.log(res))
     .catch(error => console.error(error))
