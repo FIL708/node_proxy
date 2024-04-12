5d5861f93b6c70d11d6e36048cb47be7f983f98c
Filip Zebrowski <f.zebrowski@godeltech.com>
2024-04-12 15:23:41 +0200

    PROXY-5: serv meteors data

--- src/routes/meteors.js
+++ src/routes/meteors.js
@@ -1,5 +1,24 @@
+const https = require('https')
 const { Router } = require('express')
+const config = require('config')
+const axios = require('axios')
 
-const getSomeMeteors = () => console.log("Meteors");
+const key = config.get("API_KEY")
+const url = config.get("NASA_URL")
+const fullUrl = `${url}feed?start_date=2024-04-01&end_date=2024-04-05&api_key=${key}`
+
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false
+  });
+
+const getSomeMeteors = async (req, res) => {
+    try {
+        const response = await axios.get(fullUrl, {httpsAgent})
+
+        return res.status(200).json(response.data)
+    } catch (error) {
+        return res.status(500).json({error})
+    }
+};
 
 module.exports = Router().get('/', getSomeMeteors)
