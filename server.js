15cf822a3fb839484659fd97afaab80de04e8382
Filip Zebrowski <f.zebrowski@godeltech.com>
2024-04-12 13:38:01 +0200

    PROXY-2: add https agent

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const axios = require('axios')
+const https = require('https')
+
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false
+  });
 
 const URL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-04-01&end_date=2024-04-05&api_key=8myaceLy6jnnDfZjLKb610ktfDpPc9ZKGcKMJbeX"
 
-axios.get(URL, { rejectUnauthorized: false })
+axios.get(URL, { httpsAgent })
     .then(res => console.log(res))
     .catch(error => console.error(error))
