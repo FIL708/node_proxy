e08d3f0eab803cd67ba13d1bb32f301c0f28deaf
Filip Zebrowski <f.zebrowski@godeltech.com>
2024-04-12 13:12:10 +0200

    PROXY-2: fetch data

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+const axios = require('axios')
+
+const URL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2024-04-01&end_date=2024-04-05&api_key=8myaceLy6jnnDfZjLKb610ktfDpPc9ZKGcKMJbeX"
+
+axios.get(URL, { rejectUnauthorized: false })
+    .then(res => console.log(res))
+    .catch(error => console.error(error))
