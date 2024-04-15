defbfdb02944064734cfe2aab4c57d9250650311
Filip Zebrowski <f.zebrowski@godeltech.com>
2024-04-15 11:20:02 +0200

    PROXY-5: change env variable naming

--- src/routes/meteors.js
+++ src/routes/meteors.js
@@ -4,18 +4,17 @@ const config = require('config')
 const axios = require('axios')
 
 const key = config.get("API_KEY")
-const url = config.get("NASA_URL")
+const url = config.get("API_URL")
 const fullUrl = `${url}feed?start_date=2024-04-01&end_date=2024-04-05&api_key=${key}`
 
 const httpsAgent = new https.Agent({
     rejectUnauthorized: false
   });
-
 const getSomeMeteors = async (req, res) => {
     try {
         const response = await axios.get(fullUrl, {httpsAgent})
 
-        return res.status(200).json(response.data)
+        return res.json(response.data)
     } catch (error) {
         return res.status(500).json({error})
     }
