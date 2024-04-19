f229d27f6c9306c82483b74766adb10d4af41316
Filip Zebrowski <f.zebrowski@godeltech.com>
2024-04-19 16:03:22 +0200

    PROXY-10: setup nunjukcks

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const config = require("config");
+const nunjucks = require("nunjucks");
 const router = require("./routes");
 const errorHandler = require("./middleware/error-handler.middleware.js");
 
 const PORT = config.get("PORT");
+const app = express();
 
-express()
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
+app
   .use("/", router)
   .use(errorHandler)
   .listen(PORT, () => console.log(`Server started at: ${PORT}`));
