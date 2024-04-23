c0da49c0365fa09f8ecfe6027cc5b9ea03d04acd
Filip Zebrowski <f.zebrowski@godeltech.com>
2024-04-23 14:29:56 +0200

    PROXY-10: setup not found page

---
+++ src/middleware/not-found.middleware.js
@@ -0,0 +1,6 @@
+const notFound = (req, res) => {
+  const { originalUrl } = req;
+  res.status(404).render("pages/not-found", { url: originalUrl });
+};
+
+module.exports = notFound;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
+const path = require("path");
 const express = require("express");
 const config = require("config");
 const nunjucks = require("nunjucks");
 const router = require("./routes");
 const errorHandler = require("./middleware/error-handler.middleware.js");
+const notFound = require("./middleware/not-found.middleware.js");
 
 const PORT = config.get("PORT");
 const app = express();
 
 nunjucks.configure("src/views", {
-    autoescape: true,
-    express: app,
+  autoescape: true,
+  express: app,
 });
 
-app.set("view engine", "njk")
-    .use("/", router)
-    .get("/", (req, res) => res.render("pages/home", { title: "HOME" }))
-    .use(errorHandler)
-    .listen(PORT, () => console.log(`Server started at: ${PORT}`));
+app
+  .set("view engine", "njk")
+  .use(express.static(path.join(__dirname, "public")))
+  .use("/", router)
+  .get("/", (req, res) => res.render("pages/home"))
+  .use(errorHandler)
+  .use(notFound)
+  .listen(PORT, () => console.log(`Server started at: ${PORT}`));
