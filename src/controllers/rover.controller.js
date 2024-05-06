dec7c7a12f8e19545836fc33ca98be95384e6010
Filip Zebrowski <f.zebrowski@godeltech.com>
2024-05-06 17:05:53 +0200

    PROXY-15 test

--- src/controllers/rover.controller.js
+++ src/controllers/rover.controller.js
@@ -5,6 +5,7 @@ const getForm = (req, res) => res.render('pages/rover-form');
 const postRover = async (req, res, next) => {
     try {
         const { userName, userId, apiKey } = req.body;
+
         console.log(userName, userId, apiKey);
 
         const photos = await roversService.getLastRoverPhotos();
